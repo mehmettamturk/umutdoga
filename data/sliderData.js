eef1e3972f759eb0b2a1438296e68edbba170c0b
Mehmet Tamtürk <mehmettamturk@hotmail.com.tr>
2012-07-27 22:30:06 +0300

    Added slider data.

---
+++ data/sliderData.js
@@ -0,0 +1,170 @@
+var sliderMarkup =
+    '<div class="container">' +
+        '<div class="l-rotator">' +
+            '<div class="screen">' +
+                '<noscript><img src="lib/jquery-list-slider/images/hk.jpg" alt=""/></noscript>' +
+            '</div>' +
+            '<div class="thumbnails">' +
+                '<ul>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                            '<img src="lib/jquery-list-slider/images/thumbs/hk.jpg" alt=""/>' +
+                            '<p><span class="cap-title">Hong Kong</span><br/>Lorem ipsum per augue platonem gubergren at, affert signiferumque in.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/hk.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:233px; left:0px; width:600px; height:auto;">' +
+                            '<span class="p-title">jQuery List Rotator</span><br/>' +
+                            'Lorem ipsum eos et nonumy delenit, ea nam petentium suavitate, in utinam torquatos cum.' +
+                            'Tritani facilisi at pro, tation quaeque usu at, kasd ullum fuisset at nam. No sed decore.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                            '<img src="http://www.omerbozalan.com" alt=""/>' +
+                            '<p><span class="cap-title">Bridge</span><br/>Lorem ipsum sonet nemore ancillae ne qui.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/bridge.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:198px; left:270px; width:325px; height:auto;">' +
+                            '<span class="p-title">jQuery List Rotator</span><br/>' +
+                            'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                            'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                            '<img src="http://www.omerbozalan.com" alt=""/>' +
+                            '<p><span class="cap-title">Osaka</span><br/>Lorem ipsum ne nam possit similique moderatius, ei.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/osaka.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:233px; left:0px; width:600px; height:auto;">' +
+                            '<span class="p-title">jQuery List Rotator</span><br/>' +
+                            'Lorem ipsum eos et nonumy delenit, ea nam petentium suavitate, in utinam torquatos cum.' +
+                            'Tritani facilisi at pro, tation quaeque usu at, kasd ullum fuisset at nam. No sed decore.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                            '<img src="lib/jquery-list-slider/images/thumbs/redtail.jpg" alt=""/>' +
+                            '<p><span class="cap-title">Catfish</span><br/>Lorem ipsum offendit invenire expetenda vim ei.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/redtail.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:5px; left:5px; width:325px; height:auto;">' +
+                            '<span class="p-title">jQuery List Rotator</span><br/>' +
+                            'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                            'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/jfish.jpg" alt=""/>' +
+                        '<p><span class="cap-title">Jellyfish</span><br/>Lorem ipsum per augue platonem gubergren at, affert signiferumque in.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/jfish.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:5px; left:5px; width:325px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                        'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                            '<img src="lib/jquery-list-slider/images/thumbs/sf.jpg" alt=""/>' +
+                            '<p><span class="cap-title">San Francisco</span><br/>Lorem ipsum sonet nemore ancillae ne qui.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/sf.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:233px; left:0px; width:600px; height:auto;">' +
+                            '<span class="p-title">jQuery List Rotator</span><br/>' +
+                            'Lorem ipsum eos et nonumy delenit, ea nam petentium suavitate, in utinam torquatos cum.' +
+                            'Tritani facilisi at pro, tation quaeque usu at, kasd ullum fuisset at nam. No sed decore.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/room.jpg" alt=""/>' +
+                        '<p><span class="cap-title">Living room</span><br/>Lorem ipsum sed cu malis.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/room.jpg" title="building"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:5px; left:5px; width:325px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                        'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/train.jpg" alt=""/>' +
+                        '<p><span class="cap-title">Bullet train</span><br/>Lorem ipsum offendit invenire expetenda vim ei.</p>' +
+                        '</div>' +
+                        '<a href="images/lib/jquery-list-slider/train.jpg" title=""></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:198px; left:270px; width:325px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                        'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/triworks_abstract17.jpg" alt=""/>' +
+                        '<p><span class="cap-title">3D abstract art</span><br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/triworks_abstract17.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:86px; left:5px; width:350px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum usu te solum regione percipit. Iriure virtute omittam pri ut, et his nibh scaevola, tritani officiis ocurreret ius eu.' +
+                        'Eos id elitr electram liberavisse, tantas propriae elaboraret ut mel, discere scribentur necessitatibus id cum.' +
+                        'Rebum omnium qualisque qui ne.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/triworks_abstract26.jpg" alt=""/>' +
+                        '<p><span class="cap-title">3D abstract art</span><br/>Lorem ipsum sonet nemore ancillae ne qui.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/triworks_abstract26.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:86px; left:5px; width:350px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum usu te solum regione percipit. Iriure virtute omittam pri ut, et his nibh scaevola, tritani officiis ocurreret ius eu.' +
+                        'Eos id elitr electram liberavisse, tantas propriae elaboraret ut mel, discere scribentur necessitatibus id cum.' +
+                        'Rebum omnium qualisque qui ne.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/madness_arch2.jpg" alt=""/>' +
+                        '<p><span class="cap-title">Architecture</span><br/>Lorem ipsum sonet nemore ancillae ne qui.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/madness_arch2.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:5px; left:5px; width:325px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                        'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                    '<li>' +
+                        '<div class="thumb">' +
+                        '<img src="lib/jquery-list-slider/images/thumbs/highway.jpg" alt=""/>' +
+                        '<p><span class="cap-title">Highway</span><br/>Lorem ipsum sonet nemore ancillae ne qui.</p>' +
+                        '</div>' +
+                        '<a href="lib/jquery-list-slider/images/highway.jpg"></a>' +
+                        '<a href="http://www.omerbozalan.com" target="_blank"></a>' +
+                        '<div style="top:5px; left:5px; width:325px; height:auto;">' +
+                        '<span class="p-title">jQuery List Rotator</span><br/>' +
+                        'Lorem ipsum elitr fastidii pro et, quo no idque option efficiantur.' +
+                        'Has id amet legimus vocibus, ius choro scribentur no. Autem facer eripuit cu mea. Vim an malorum qualisque vituperata.' +
+                        '</div>' +
+                    '</li>' +
+                '</ul>' +
+            '</div>' +
+        '</div>' +
+    '</div>';
